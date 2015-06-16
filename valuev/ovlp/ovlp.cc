c22c4d93340dd75c2e6e7453caca5a29f2ddddea
Darshan Washimkar <darshan.wash@gmail.com>
2015-06-16 15:02:08 -0600

    deleted unnessary code

--- valuev/ovlp/ovlp.cc
+++ valuev/ovlp/ovlp.cc
@@ -15,9 +15,9 @@ using namespace std;
 
 int main(int argc, char *argv[])    
 {
-    double readarry1[] = {1.843, 4.843, 10.593, 6.938, 8.292, 10.711, 5.367, 8.440, 6.335, 1.947, 3.756, 10.678, 6.969, 2.964, 6.493, 8.316, 5.583, 9.149, 8.271, 2.456, 1.564, 9.179, 34.473, 2.682, 13.115, 6.171, 4.170, 22.259, 3.319, 2.181};
+    double readarry1[] = {8.271, 2.456, 1.564, 9.179, 34.473, 2.682, 13.115, 6.171, 4.170, 22.259, 3.319, 2.181, 7.171, 3.170, 12.259, 9.319, 12.181};
 
-    double readarry2[] = {1.843, 8.292, 10.711, 5.367, 8.440, 6.335, 1.947, 3.756, 10.678, 6.969, 2.964, 6.493, 8.316, 5.583, 9.149, 8.271, 2.456, 1.564, 9.179, 34.473, 2.682, 13.115, 6.171, 4.170, 22.259, 3.319, 2.181, 10.843, 2.593, 10.938};
+    double readarry2[] = {11.212, 8.271, 2.456, 1.564, 9.179, 34.473, 2.682, 13.115, 6.171, 4.170, 22.259, 3.319, 2.181, 7.171, 3.170, 12.259, 9.319, 12.181};
     om_read read1, read2;
     read1.read_name = "5038535_0_1317";
     read1.Enz_name = "SpeI";
@@ -44,21 +44,7 @@ int main(int argc, char *argv[])
 
 
     scoring_params sp(.2,1.2,.9,7,17.43,0.58, 0.0015, 0.8, 1, 3);
-    //sp.init();
 
-
-
-//    for(int i=start_num; i<end_num; i++){
-//      for(int j=0; j<i/*maps.collection.size()*/; j++){
-//	if(i!=j && maps.collection[i].map_read.size() > 5
-//	   && maps.collection[j].map_read.size() > 5){
-	  	  
-	  cout<<endl;
-	  
-
-
-//	  cout<<"start:"<<start_num<<" end:"<<end_num;
-//	  cout<<" cur: "<<i<<"->"<<j<<endl;
 	  om_read tar_map = read1; //maps.collection[i];
 	  om_read for_map = read2; //maps.collection[j];
 	  om_read rev_map = for_map.reverse();
@@ -66,16 +52,9 @@ int main(int argc, char *argv[])
 	  rm_alignment for_alignment(tar_map, for_map, sp);
 	  rm_alignment rev_alignment(tar_map, rev_map, sp);
 
-	  //for_alignment.localized_overlap_alignment
-	  //  (0,tar_map.map_read.size(),0,for_map.map_read.size());
-	  //rev_alignment.localized_overlap_alignment
-	  //  (0,tar_map.map_read.size(),0,rev_map.map_read.size());
 
 	  for_alignment.optimized_overlap_alignment();
 	  rev_alignment.optimized_overlap_alignment();
-
-	  //for_alignment.overlap_alignment();
-	  //rev_alignment.overlap_alignment();
 
 	  for_alignment.overlap_t_score();
 	  rev_alignment.overlap_t_score();
@@ -85,9 +64,6 @@ int main(int argc, char *argv[])
 
 	  double for_t_score = for_alignment.Tmax;
 	  double rev_t_score = rev_alignment.Tmax;
-
-	  //double for_p_value = for_alignment.ovlp_p_value();
-	  //double rev_p_value = rev_alignment.ovlp_p_value();
 
 	  double for_ovlp_size = for_alignment.ovlp_size();
 	  double rev_ovlp_size = rev_alignment.ovlp_size();
@@ -140,10 +116,7 @@ int main(int argc, char *argv[])
 	      cout<<" ";
 	      cout<<for_alignment.tar_restr_al_sites[k];
 	    }
-	    //cout<<tar_map.map_read.size()<<" ";
-	    //cout<<for_map.map_read.size()<<" ";
-	    //cout<<ovlp_start1<<" "<<ovlp_end1<<" ";
-	    //cout<<ovlp_start2<<" "<<ovlp_end2<<endl;
+
 	    cout<<endl<<endl;
 
 	    //if(for_score>score_thresh)
@@ -183,20 +156,13 @@ int main(int argc, char *argv[])
 	      cout<<" ";
 	      cout<<rev_alignment.tar_restr_al_sites[k];
 	    }
-	    //cout<<tar_map.map_read.size()<<" ";
-	    //cout<<(rev_map.map_read.size())<<" ";
-	    //cout<<ovlp_start1<<" "<<ovlp_end1<<" ";
-	    //cout<<ovlp_start2<<" "<<ovlp_end2<<endl;
+
 	    cout<<endl<<endl;
 	    	    
-	    //if(rev_score > score_thresh)
-	    //if(rev_t_score > t_score_thresh)
 	    rev_alignment.output_alignment(cout);
 	    rev_alignment.output_alignment(det_str);
 	  }
-//	}
-//      }
-//    }
+
     
     det_str.close();
   
